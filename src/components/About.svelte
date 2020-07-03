edf8728e9a13c05ca645654b7dee6a2ba8a4fff9
Jeferson Ferreira <jeferson0993@gmail.com>
2020-07-03 18:35:08 -0300

    Update About.svelte

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -2,6 +2,77 @@
 </script>
 
 <h1>About</h1>
+<p align="center">
+  <a href="https://github.com/othneildrew/Best-README-Template">
+    <img src="https://user-images.githubusercontent.com/29678099/71330693-1ed06d80-250d-11ea-9b98-a04279392272.jpeg" alt="Logo" width="80" height="80" style="border-radius: 50%;">
+  </a>
+
+  <h2 align="center">BLOG with Svelte 3</h2>
+
+  <p align="center">
+    Published with Now, design with materializer e interact with Jsonplaholder api.
+    <br />
+    <a href="https://github.com/jeferson0993/svelte-blog/"><strong>View Code</strong></a>
+    <br />
+    <br />
+    <a href="https://github.com/jeferson0993/svelte-blog/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/jeferson0993/">Some Others Repos</a>
+  </p>
+</p>
+
+<br />
+<!-- ABOUT THE PROJECT -->
+<h3>About The Project</h3>
+<p>I just have doing an Svelte 3 example blog that have:</p>
+<ol>
+    <li>Header with the menu or navigation bar</li>
+    <li>A fake login with 10 diferent random users got from the api</li>
+    <li>Main content area with all latest blog posts or posts by the logged in user</li>
+    <li>An article page to show details about the chosen post</li>
+    <li>A profile page with details about the current user</li>
+    <li>A About page with technical information about the technologies used</li>
+    <li>Footer with relevant links and contacts from me</li>
+</ol>
+
+<br />
+<h3>Built With</h3>
+<p>Which Languages and Libraries are used :</p>
+<ul>
+  <li><a href="https://svelte.dev/">[( Svelte 3 )]</a></li>
+  <li><a href="https://materializecss.com/about.html">[( Materialize CSS )]</a></li>
+  <li><a href="https://jsonplaceholder.typicode.com/">[( Jsonplaceholder api ])</a></li>
+  <li><a href="https://www.npmjs.com/package/page">([ Router with Pages.js ])</a></li>
+  <li><a href="https://vercel.com/home">[( Deploy with Vercel )]</a></li>
+</ul>
+<br />
+
+<!-- ROADMAP -->
+<h3>Roadmap</h3>
+<p>See the <a href="https://github.com/jeferson0993/svelte-blog/issues">([ open issues ])</a> for a list of proposed features (and known issues).</p>
+
+<br />
+<!-- CONTRIBUTING -->
+<h3>Contributing</h3>
+<p>Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are <b>greatly appreciated</b>.</p>
+<p>
+1. Fork the Project<br />
+2. Create your Feature Branch (<prep>git checkout -b feature/AmazingFeature</prep>)<br />
+3. Commit your Changes (<prep>git commit -m 'Add some AmazingFeature'</prep>)<br />
+4. Push to the Branch (<prep>git push origin feature/AmazingFeature</prep>)<br />
+5. Open a Pull Request
+</p>
+
+<br />
+<!-- LICENSE -->
+<h3>License</h3>
+<p>Distributed under the MIT License. See `LICENSE` for more information.</p>
+
+<br />
+<!-- CONTACT -->
+<h3>Contact</h3>
+<p>Jeferson Ferreira - jeferson0993@gmail.com</p>
+<p>Project Link: <a href="https://github.com/jeferson0993/svelte-blog">([ Repository ])</a></p>
 
 <style>
 </style>
